0511a456eb1751d442f12ea3c9b5c57e69981031
mihneacojocaru <mihnea.co@gmail.com>
2022-05-30 20:35:55 +0200

    first commit

---
+++ src/pages/Orders.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { GridComponent, ColumnsDirective, ColumnDirective, Resize, Sort, ContextMenu, Filter, Page, ExcelExport, PdfExport, Edit, Inject } from '@syncfusion/ej2-react-grids';
+
+import {ordersData, contextMenuItems, ordersGrid} from '../data/dummy';
+
+import { Header } from '../components';
+
+
+const Orders = () => {
+  return (
+    <div> 
+    <div className='m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl'>
+      <Header category="Page" title="Orders"/>
+      <GridComponent
+        id="gridcomp"
+        dataSource={ordersData}
+        allowPaging
+        allowSorting
+      >
+        <ColumnsDirective>
+          {ordersGrid.map((item,index)=>(
+            <ColumnDirective key={index} {...item}/>
+          ))}
+        </ColumnsDirective>
+        <Inject services={[Resize, Sort, ContextMenu, Filter, Page, ExcelExport, Edit, PdfExport]}/>
+      </GridComponent>
+    </div>
+    <div className="flex justify-center m-10 mt-24">
+    <p>© 2022 <a className="text-blue-500" href="https://mihneacojocaru.github.io/" target="_blank" rel="noreferrer">Mihnea Cojocaru</a></p>
+  </div>
+    </div>
+  )
+}
+
+export default Orders
